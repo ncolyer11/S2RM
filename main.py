8b9e9ebc02b54b9af6c65eb9717d2274585e2698
ncolyer <emeraldcreeper7@outlook.com>
2025-04-09 01:45:46 +1000

    added a TODO item to ensure consistency in update/decline buttons for update prompting

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from src.config import check_has_selected_mc_vers, update_config
 # XXX issue when importing a lot of files sometimes the loading bar doesnt show after a bit
 # but mainly the input materials list doesnt get reset and new stuff just gets appended to the end?
 # - add tooltip to show all input file names when they get truncated
+# - make yes no update/decline buttosn consistent for update prompting
 def main():
     # Check if the user's program or mc version needs updating/downloading
     update_config(redownload=False, delete=True)
